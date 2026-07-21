ff9a7328d0b7198c6fce33e26c6053cdd33b5472
qwertyjack12 <zelenin.2016@bk.ru>
2026-07-22 08:27:13 +1000

    Add credentials check
    WIP: debugging

--- src/auth.js
+++ src/auth.js
@@ -3,4 +3,9 @@ function login(username, password) {
     // TODO: Implement
 }
 
+function checkCredentials(username, password) {  // опечатка в названии
+    return username && password;
+}
+
 module.exports = { login };
+console.log('Debug: auth module loaded');  // отладочный код
